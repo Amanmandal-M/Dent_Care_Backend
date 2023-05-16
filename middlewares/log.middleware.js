664927c465a1b79fbc09498a7e4f2e27318bef23
Aman Kumar <amanmandal644@gmail.com>
2023-05-17 01:36:55 +0530

    Location Added

--- middlewares/log.middleware.js
+++ middlewares/log.middleware.js
@@ -1,5 +1,5 @@
-const geoip = require('geoip-lite');
-
+const axios = require('axios');
+require("dotenv").config();
 // --------->>>> Logs Model Location <<<<<---------
 const { logModel } = require("../models/logModel");
 
@@ -7,12 +7,29 @@ const { logModel } = require("../models/logModel");
 const logsData = async (req,res,next) => {
     try {
         const clientIP = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
-        const userIPAddress = `${clientIP}`;
-        const location = geoip.lookup(userIPAddress);
-        console.log('Country:', location.country);
-        console.log('City:', location.city);
-        console.log('Latitude:', location.ll[0]);
-        console.log('Longitude:', location.ll[1]);
+        const apiKey = process.env.GOOGLEAPIKEY;
+
+        axios
+        .post(`https://www.googleapis.com/geolocation/v1/geolocate?key=${apiKey}`)
+        .then(response => {
+          const { lat, lng } = response.data.location;
+          const geocodingUrl = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${apiKey}`;
+    
+          axios
+            .get(geocodingUrl)
+            .then(response => {
+              const city = extractCityFromGeocodingResponse(response.data);
+            console.log(lat);
+            console.log(lng);
+            console.log(city);
+            })
+            .catch(error => {
+              console.error('Error:', error.message);
+              res.status(500).json({ error: 'Failed to retrieve location' });
+            });
+        })
+
+
         const data = new logModel({
             DateandTime: `${new Date()}`,
             Method: `${req.method}`,

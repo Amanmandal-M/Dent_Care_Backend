429c2114ac073903ea1920f7ec374ec00497fa20
Aman Kumar <amanmandal644@gmail.com>
2023-05-17 01:12:35 +0530

    Location Added

--- middlewares/log.middleware.js
+++ middlewares/log.middleware.js
@@ -1,3 +1,5 @@
+const geoip = require('geoip-lite');
+
 // --------->>>> Logs Model Location <<<<<---------
 const { logModel } = require("../models/logModel");
 
@@ -5,12 +7,16 @@ const { logModel } = require("../models/logModel");
 const logsData = async (req,res,next) => {
     try {
         const clientIP = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+        const userIPAddress = `${clientIP}`;
+        const location = geoip.lookup(userIPAddress);
+        console.log(location);
         const data = new logModel({
             DateandTime: `${new Date()}`,
             Method: `${req.method}`,
             URL: `${req.url}`,
             IP: `${clientIP}`
         })
+
         await data.save();
         next();
     } catch (error) {
